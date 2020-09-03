b2b1bb7613e492628167e45822154a5f47ebcdeb
Nahui068 <kyjjj105@naver.com>
2020-09-03 15:57:41 +0900

    네이버로그인

--- src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
@@ -1,10 +1,15 @@
 package com.jojoldu.book.springboot;
 
+import com.jojoldu.book.springboot.config.auth.SecurityConfig;
 import com.jojoldu.book.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.is;
@@ -22,13 +27,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 // @Service, @Component, @Repository등은 사용 불가능
 // 여기서는 Controller만 사용하기 때문에 선언
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class , excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                                classes = SecurityConfig.class)
+})
 public class HelloControllerTest {
 
     @Autowired // 스프링이 관리하는 빈을 주입 받음
     private MockMvc mvc; // 웹 API를 테스트할 때 사용, 스프링 MVC 테스트의 시작점
     // 이 클래스 통해 HTTP GET,POST 등에 대한 API테스트 가능
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello가_리턴된다() throws Exception{
         String hello = "hello";
@@ -41,7 +50,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello)); // mvc.perform의 결과를 검증, 응답 본문의 내용을 검증
         // Controller에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증!!
     }
-
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception{
         String name = "hello";
