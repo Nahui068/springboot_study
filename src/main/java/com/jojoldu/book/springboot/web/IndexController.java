5d0d3e32c24c4f49a2528091f8de7bb5aa71a9f7
Nahui068 <kyjjj105@naver.com>
2020-09-03 13:22:21 +0900

    로그인_어노테이션

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,6 +1,8 @@
 package com.jojoldu.book.springboot.web;
 
 
+import com.jojoldu.book.springboot.config.auth.LoginUser;
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,17 +11,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){ // 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음
+    // @LoginUser SessionUser user : 기존에 httpSession.getAttribute("user")로 가져오던 세션 정보 값이 개선
+    // 이제는 어느 컨트롤러든지 @LoginUser만 사용하면 세션정보를 가져올 수 있음
+    public String index(Model model, @LoginUser SessionUser user){
 
         // postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
         model.addAttribute("posts",postsService.findAllDesc());
+
+        // 세션에 저장된 값이 있을 때만 model에 userName으로 등록
+        // 세션에 저장된 값이 없으면 model엔 아무런 값이 없는 상태이니 로그인 버튼이 보이게 됨
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+        }
+
         return "index";
     }
 
@@ -35,5 +49,7 @@ public class IndexController {
 
         return "posts-update";
     }
+
+
 }
 
